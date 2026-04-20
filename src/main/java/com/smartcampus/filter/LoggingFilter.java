787529cbe08c7e86bbb078195fbc5b0fa2482219
Upethna <upethnaweerasekara@gmail.com>
2026-04-20 17:49:38 +0530

    Add request/response logging filter for API observability

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.filter;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author HP
+ */
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info(String.format("Incoming: [%s] %s",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri()));
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info(String.format("Outgoing: Status [%d] for [%s] %s",
+                responseContext.getStatus(),
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri()));
+    }
+}
